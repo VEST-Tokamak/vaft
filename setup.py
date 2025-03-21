f300201774365ae2fe6babdc7daad3fccca9218a
이선재 <138286329+satelite2517@users.noreply.github.com>
2025-03-21 09:03:39 +0900

    setup for pypi

--- setup.py
+++ setup.py
@@ -1,37 +1,26 @@
-# -----------------------------------
-# Instructions for installing this package:
-# 0. Run the required installations:
-#    Run the following command in the terminal:
-#    $ pip install -r requirements.txt
-# 1. install VEST package:
-#    $ pip install .
-#   (if you install VEST package in editable mode for dev, Use the `-e` option: $ pip install -e .)
-# Once installed, you can use the 'VEST' package and its associated dependencies in your project.
-# -----------------------------------
-
 from setuptools import setup, find_packages
 
 setup(
     name="vaft",
     version="0.1",
     packages=find_packages(),
-
-    # These are commented out because the dependencies are already installed in the requirements.txt file.
-    # install_requires=[
-    #     "h5py",
-    #     "numpy",
-    #     "uncertainties",
-    #     "omas",
-    #     "matplotlib",
-    # ],
-    # dependency_links=[
-    #     "git+https://github.com/hdfgroup/h5pyd.git#egg=h5pyd",
-    # ],
-    # extras_require={
-    #     #    $ pip install .[dev] 
-    #     "dev": [
-    #         "os",
-    #         "subprocess",
-    #     ],
-    # },
+    description="Versatile Analytical Framework for Tokamak",
+    author="VEST team",
+    author_email="satelite2517@snu.ac.kr",
+    url="https://github.com/VEST-Tokamak/vaft",
+    install_requires=[
+        "h5py",
+        "numpy",
+        "uncertainties",
+        "omas",
+        "matplotlib",
+        "h5pyd @ git+https://github.com/hdfgroup/h5pyd.git",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "black",
+            "flake8",
+        ],
+    },
 )
